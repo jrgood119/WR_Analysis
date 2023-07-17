648d5d230d950c4d26bedb6c1c3bc737048945c7
JT Good <jrgood119@gmail.com>
2023-07-16 20:58:42 -0500

    Fixes to PreProcess and added To_Do

--- PreProcess.py
+++ PreProcess.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-class PreProcess:
-    """This class takes an Excel input and converts it to a usuable format for analysis"""
+#class PreProcess:
+"""This class takes an Excel input and converts it to a usuable format for analysis"""
 
-    def __init__(self):
-        self.raw_df = pd.read_excel('WR_Edits.xlsx')
+def read_data():
+    df = pd.read_excel('/Users/jtgood/Desktop/Python/WR_Model/WR_Edits.xlsx', index_col='Name')
+    print(df.columns)
+    df = df.drop(columns=['cfb_id','NFLfastR','PFF','PFR_ID'])
+    print(df.columns)
+    return df
 
-        return raw_df
-    
-
-if __name__ == '__main__':
-    print(PreProcess)
+print(read_data())
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WR_Analysis
+
+Project that starts with looking at Combine statistics and draft history to see if there is a correlation
