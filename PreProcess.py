a2c0d3cc1d9096cd48211e269a37babce92e10ba
JT Good <jrgood119@gmail.com>
2023-07-17 20:10:56 -0500

    Added additional functions/to do items

--- PreProcess.py
+++ PreProcess.py
@@ -1,13 +1,25 @@
 import pandas as pd
 
-#class PreProcess:
-"""This class takes an Excel input and converts it to a usuable format for analysis"""
+class PreProcess:
+    """This class takes an Excel input and converts it to a usuable format for analysis"""
 
-def read_data():
-    df = pd.read_excel('/Users/jtgood/Desktop/Python/WR_Model/WR_Edits.xlsx', index_col='Name')
-    print(df.columns)
-    df = df.drop(columns=['cfb_id','NFLfastR','PFF','PFR_ID'])
-    print(df.columns)
-    return df
+    def __init__(self):
+        pass
 
-print(read_data())
+    def read_data():
+        """This functions reads in excel data to be used and drops unwanted columns"""
+        df = pd.read_excel('/Users/jtgood/Desktop/Python/WR_Model/WR_Edits.xlsx', index_col='Name')
+        print(df.columns)
+        df = df.drop(columns=['cfb_id','NFLfastR','PFF','PFR_ID'])
+        print(df.columns)
+        return df
+    
+    def remove_nulls(df):
+        """This function will remove nulls from the data"""
+
+
+    def set_types():
+        """This functions converts data types to usable formats"""
+
+
+print(PreProcess.read_data())
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # WR_Analysis
 
 Project that starts with looking at Combine statistics and draft history to see if there is a correlation
+
+## Data Sources
+Combine Data:
+Peter Howard (https://www.patreon.com/m/1433424/posts)
+https://www.nfl.com/draft/
+
+RAS scores:
+https://ras.football/
+
+### PreProcess class
